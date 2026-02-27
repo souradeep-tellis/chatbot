2df24bb6838a2326c71231f99c0927caf2048963
Bubun <missiondevtarget@gmail.com>
2026-02-27 20:58:05 +0530

    Update streamlist_frontend_backend_database.py

--- streamlist_frontend_backend_database.py
+++ streamlist_frontend_backend_database.py
@@ -1,7 +1,66 @@
 import streamlit as st
-from langgraph_database_backend import chatbot,llm,retrieve_all_threads
 from langchain_core.messages import HumanMessage
 import uuid
+
+from langgraph.graph import StateGraph,START,END
+from pydantic import BaseModel,Field
+from typing import TypedDict,Literal,Annotated
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage,HumanMessage,BaseMessage
+from dotenv import load_dotenv
+import operator
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
+import sqlite3
+import os
+
+
+
+from langgraph.graph import add_messages
+class chatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+model_url=os.getenv("X_URL")
+load_dotenv()
+# llm=ChatOpenAI(model="gpt-4o-mini")
+# llm=ChatGoogleGenerativeAI(model="gemini-flash-lite-latest")
+#This is my azure server
+# http://20.193.153.224:11434
+llm=ChatOllama(
+    model="qwen3-vl:235b-cloud",           
+    base_url=model_url,
+    validate_model_on_init=True)
+
+def chat_node(state:chatState):
+    messages=state['messages']
+    
+    response=llm.invoke(messages)
+    
+    return {'messages': [response] }
+
+
+# here instead of MemorySaver we can also use SqliteSaver or any other saver from langgraph.checkpoint
+conn=sqlite3.connect('chatbot.db',check_same_thread=False)
+
+checkpointer=SqliteSaver(conn)
+graph=StateGraph(chatState)
+
+graph.add_node("chat_node",chat_node)
+
+graph.add_edge(START,"chat_node")
+graph.add_edge("chat_node",END)
+
+chatbot=graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads=set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
+
 
 
 # ***********************Utility function **************************
@@ -139,3 +198,4 @@ if user_input:
 
     if len( st.session_state["message_history"]) >1:
         st.rerun()
+
